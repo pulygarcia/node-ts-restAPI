bea69acf330c9b801b3a084aa572e4dfd6a9c679
puly garcia <pulygarcia09@gmail.com>
2024-07-04 18:53:44 -0300

    connection with database, adding .env template as a guide

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,3 +1,14 @@
+import mongoose from "mongoose";
+import colors from 'colors'
+
 export const db = async () => {
-    console.log('DB connection');
+    //console.log('DB connection');
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}`);
+
+        console.log(colors.bgGreen('Connected successfully to Database'));
+    } catch (error:any) {
+        console.log(colors.bgRed(error.message));
+        process.exit(1);
+    }
 }
